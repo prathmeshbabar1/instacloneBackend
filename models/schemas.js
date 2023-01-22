069b2631005055d5599b9c0c8823742163703912
prathmesh babar <prathmeshbabar1997@gmail.com>
2023-01-22 23:11:13 +0530

    added backend

---
+++ models/schemas.js
@@ -0,0 +1,40 @@
+const mongoose = require('mongoose');
+const UserSchema = mongoose.Schema({
+    username:{
+        type:String,
+        require:true,
+        default:"preeti"
+    },
+    password:{
+        type:String,
+        require:true,
+        default:"gxhdjthd"
+    }
+})
+const PostSchema = mongoose.Schema({
+    username:{
+        type:String,
+        require:true,
+        default:"name1"
+    },
+    address:{
+        type:String,
+        // require:true,
+        default: 'Hyderabad'
+    },
+    image_file: {
+        type: String ,
+        // require: true 
+    },
+    description: {
+        type: String ,
+        // require: true 
+    },
+    like:{
+type:String
+    }
+
+})
+
+module.exports = {User: mongoose.model("InstaUser", UserSchema),
+                  Post:mongoose.model("InstaPost", PostSchema) }
